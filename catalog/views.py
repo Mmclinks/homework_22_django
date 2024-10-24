7a8548835e9beb9dbaa35c508ee58d36ab8e1303
mmclinks <mmclinks.ml@gmail.com>
2024-10-24 20:46:59 +0300

    добавлена Ссылки на товары, возврат

--- catalog/views.py
+++ catalog/views.py
@@ -1,43 +1,11 @@
-# import messages
-# from catalog.models import Product
-# from django.shortcuts import render, redirect
-# from django.contrib import messages
-# from .models import Contact
-#
-#
-# def home(request):
-#     latest_products = Product.objects.all()
-#     return render(request, "home.html", {"latest_products": latest_products})
-#
-#
-# def contacts(request):
-#     if request.method == "POST":
-#         name = request.POST.get("name")
-#         phone = request.POST.get("phone")
-#         message = request.POST.get("message")
-#
-#         # Сохранение данных в базе данных
-#         contact = Contact(name=name, phone=phone, message=message)
-#         contact.save()
-#
-#         # Отправляем сообщение об успешной отправке
-#         messages.success(request, "Ваше сообщение успешно отправлено!")
-#
-#         # Перенаправляем обратно на страницу контактов
-#         return redirect("catalog:contacts")
-#
-#     # Извлечение всех контактов для отображения
-#     contacts = Contact.objects.all()
-#
-#     return render(request, "contacts.html", {"contacts": contacts})
-#
-#
-#
-from gc import get_objects
-
 from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product
+
+
+def home(request):
+    products = Product.objects.all()
+    return render(request, "home.html", {'products': products})
 
 
 def products_list(request):
