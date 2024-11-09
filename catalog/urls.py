70a1db3708d49a01cc93c1632b2bfd53e1ef907c
alex <mmclinks.ml@gmail.com>
2024-11-09 15:17:33 +0300

    small fixes

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,25 +2,22 @@ from django.urls import path
 from catalog.apps import CatalogConfig
 from .views import ProductDeleteView, ProductUpdateView, ProductCreateView, ProductDetailView, ProductListView, \
     ContactsView
+from catalog import views
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    # Главная страница - список продуктов
     path('', ProductListView.as_view(), name='home'),
-
-    # Страница создания нового продукта
     path('product/new/', ProductCreateView.as_view(), name='product_create'),
-
-    # Страница детальной информации о продукте
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-
-    # Страница редактирования продукта
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
-
-    # Страница удаления продукта
     path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
-
-    # Страница контактов
     path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('', views.ProductListView.as_view(), name='product_list'),
+    path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('product/create/', views.ProductCreateView.as_view(), name='product_create'),
+    path('product/<int:pk>/update/', views.ProductUpdateView.as_view(), name='product_update'),
+    path('product/<int:pk>/delete/', views.ProductDeleteView.as_view(), name='product_delete'),
+    path('', ProductListView.as_view(), name='products_list'),
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),
 ]
