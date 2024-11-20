2d0d4ad49fbe1807945f91a2b5354ed7da0b9cfa
mmclinks <mmclinks.ml@gmail.com>
2024-11-20 21:18:00 +0300

    Add permissions

--- catalog/views.py
+++ catalog/views.py
@@ -1,52 +1,105 @@
-# import messages
-# from catalog.models import Product
-# from django.shortcuts import render, redirect
-# from django.contrib import messages
-# from .models import Contact
-#
-#
-# def home(request):
-#     latest_products = Product.objects.all()
-#     return render(request, "home.html", {"latest_products": latest_products})
-#
-#
-# def contacts(request):
-#     if request.method == "POST":
-#         name = request.POST.get("name")
-#         phone = request.POST.get("phone")
-#         message = request.POST.get("message")
-#
-#         # Сохранение данных в базе данных
-#         contact = Contact(name=name, phone=phone, message=message)
-#         contact.save()
-#
-#         # Отправляем сообщение об успешной отправке
-#         messages.success(request, "Ваше сообщение успешно отправлено!")
-#
-#         # Перенаправляем обратно на страницу контактов
-#         return redirect("catalog:contacts")
-#
-#     # Извлечение всех контактов для отображения
-#     contacts = Contact.objects.all()
-#
-#     return render(request, "contacts.html", {"contacts": contacts})
-#
-#
-#
-from gc import get_objects
-
-from django.shortcuts import render, get_object_or_404
-
+from django.contrib import messages
+from django.core.mail import EmailMessage
+from django.urls import reverse_lazy
+from django.views.generic import DeleteView, DetailView, FormView, ListView
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin  # Импортируем миксин
+from django.http import HttpResponseForbidden
+from catalog.forms.forms import ContactForm, ProductForm
 from catalog.models import Product
 
 
-def products_list(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, "product_list.html", context)
+class ProductListView(ListView):
+    """
+    Представление главной страницы
+    """
+    model = Product
+    template_name = "catalog/home.html"
+    context_object_name = "products"
 
 
-def products_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {'product': product}
-    return render(request, 'products_detail.html', context)
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:product_list")
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user  # Устанавливаем владельца
+        return super().form_valid(form)
+
+
+class ProductDetailView(DetailView):
+    """
+    Представление страницы товара
+    """
+    model = Product
+    template_name = "catalog/product_detail.html"
+    context_object_name = "product"
+
+
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:product_list")
+
+    def dispatch(self, request, *args, **kwargs):
+        product = self.get_object()
+        if product.owner != request.user:
+            return HttpResponseForbidden("Вы не можете редактировать этот продукт.")
+        return super().dispatch(request, *args, **kwargs)
+
+
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
+    model = Product
+    template_name = "catalog/product_confirm_delete.html"
+    success_url = reverse_lazy("catalog:product_list")
+
+    def dispatch(self, request, *args, **kwargs):
+        product = self.get_object()
+        if product.owner != request.user and not request.user.has_perm("catalog.can_unpublish_product"):
+            return HttpResponseForbidden("Вы не можете удалить этот продукт.")
+        return super().dispatch(request, *args, **kwargs)
+
+
+class ContactsView(FormView):
+    """
+    Представление страницы контактов
+    """
+    template_name = "catalog/contacts.html"
+    form_class = ContactForm
+    success_url = reverse_lazy("catalog:contacts")
+
+    def form_valid(self, form):
+        """
+        Переопределение метода для отправки письма при успешной отправки формы
+        """
+        name = form.cleaned_data["name"]
+        message = form.cleaned_data["message"]
+        subject = f"Новое сообщение от {name}"
+        recipient_list = ["lacryk@gmail.com"]
+
+        email = EmailMessage(
+            subject=subject,
+            body=message,
+            from_email="lacry@rambler.ru",
+            to=recipient_list,
+        )
+
+        email.headers = {
+            "Reply-To": "lacry@rambler.ru",
+        }
+
+        email.send(fail_silently=False)
+
+        messages.success(
+            self.request, f'Спасибо, {name}! Ваше сообщение "{message}" получено.'
+        )  # Добавляем сообщение об успехе
+        return super().form_valid(form)  # Вызовем метод родителя для перенаправления на success_url
+
+    def form_invalid(self, form):
+        """
+        Если форма недействительна, просто отобразим шаблон с ошибками
+        """
+        return super().form_invalid(form)
